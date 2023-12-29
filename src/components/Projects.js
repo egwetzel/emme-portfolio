6b62e72fdcc5c000df6e0dade8e9754875555896
egwetzel <emmegwetzel@gmail.com>
2023-12-29 18:58:05 -0500

    Added project page

---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>My Projects</h2>
+      {/* Add more content as needed */}
+    </section>
+  );
+};
+
+export default Projects;
