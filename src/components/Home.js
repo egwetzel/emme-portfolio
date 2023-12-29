8ef2ce2fcfdbd2aac2df1889b3b0255d7b42ad16
egwetzel <emmegwetzel@gmail.com>
2023-12-29 18:58:12 -0500

    added home page

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <section id="home">
+      <h2>Welcome to My Portfolio</h2>
+      {/* Add more content as needed */}
+    </section>
+  );
+};
+
+export default Home;
