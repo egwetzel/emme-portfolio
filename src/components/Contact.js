94bfc77ca7d954d00b23a7af14eab3e6a5d616eb
egwetzel <emmegwetzel@gmail.com>
2023-12-29 18:58:25 -0500

    added header component

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      {/* Add a contact form or other contact information */}
+    </section>
+  );
+};
+
+export default Contact;
