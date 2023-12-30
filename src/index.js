707648780e554dbb43cfbb7c759b43f8d4ba0a3a
egwetzel <emmegwetzel@gmail.com>
2023-12-29 19:23:23 -0500

    updated index.js to include BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ReactDOM.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
 root.render(
   <React.StrictMode>
     <App />
