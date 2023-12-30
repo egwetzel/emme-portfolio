76cf361705cc1705eaa16a8041134c399dfce31c
egwetzel <emmegwetzel@gmail.com>
2023-12-29 19:02:29 -0500

    updated copyright in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <p>&copy; 2023 Your Name. All rights reserved.</p>
+      <p>&copy; 2023 Emmeline Wetzel. All rights reserved.</p>
     </footer>
   );
 };
