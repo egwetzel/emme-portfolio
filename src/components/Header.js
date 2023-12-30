de0c77e5789cd6977bd6f604f7e720166b10866a
egwetzel <emmegwetzel@gmail.com>
2023-12-29 19:03:04 -0500

    updated name in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <h1>Your Name</h1>
+      <h1>Emmeline Wetzel</h1>
       <nav>
         <ul>
           <li><a href="#home">Home</a></li>
