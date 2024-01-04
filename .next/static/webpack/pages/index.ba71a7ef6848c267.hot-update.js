439c13eee5fdc1c503809087b14f2575b8743cc1
egwetzel <emmegwetzel@gmail.com>
2024-01-04 18:26:04 -0500

    updated portfolio data

---
+++ .next/static/webpack/pages/index.ba71a7ef6848c267.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./data/portfolio.json":
+/*!*****************************!*\
+  !*** ./data/portfolio.json ***!
+  \*****************************/
+/***/ (function(module) {
+
+module.exports = JSON.parse('{"name":"Emmeline Wetzel","headerTaglineOne":"Hi!","headerTaglineTwo":"I\'m Emmeline Wetzel","headerTaglineThree":"designer, developer, and student","headerTaglineFour":" based in Pittsburgh.","showCursor":true,"showBlog":false,"darkMode":true,"showResume":true,"socials":[{"id":"1","title":"Github","link":"https://github.com/egwetzel"},{"id":"2","title":"LinkedIn","link":"https://www.linkedin.com/in/emmeline-wetzel/"}],"projects":[{"id":"1","title":"Project One Test","description":"Web Design","imageSrc":"https://images.unsplash.com/photo-1487837647815-bbc1f30cd0d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Njl8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60","url":"http://chetanverma.com/"}],"aboutpara":"As a senior studying Information Systems, I am passionate about the intersection of technology and human desires. I have a strong interest in full stack & front-end engineering as well as design research. Through my academic coursework and extracurricular activities, I have gained technical skills in programming languages such as Java and Python, as well as experience with design principles and tools. I am eager to apply my skills and continue learning in a dynamic and challenging environment where I can collaborate with diverse teams.","resume":{"tagline":"I\'m interested in creative solutions to problems that people face everyday.","description":"I have diligently pursued pleasing the user in my design and development work.","experiences":[{"id":"1","dates":"September 2021 - Present","type":"Part Time","position":"Head Teaching Assistant","bullets":"Contribute to developing an autograder for student coding assignments in Ruby:: Lead and assist with labs, lectures, office hours, and curriculum development for 67-250 Information Systems Milieux and 67-272 Application Design and Development:: Manage and aid all personnel with their tasks "},{"id":"d495c23b-4f65-479a-9b8a-cfbc1c089725","dates":"July 2020 - March 2022","type":"Full Time","position":"Frontend Engineer at X","bullets":"Worked on the frontend of a React application, Worked on the frontend of a React application"}],"education":{"universityName":"University X","universityDate":"2016-2020","universityPara":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"},"languages":["Javascript","HTML5","CSS","Python","Go"],"frameworks":["React","Typescript","NodeJs"],"others":["Figma","AdobeXD","AWS"]}}');
+
+/***/ })
+
+});
