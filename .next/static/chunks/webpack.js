f43cd0f528083efc4a15b6b688cf54829f352554
egwetzel <emmegwetzel@gmail.com>
2024-01-04 19:03:13 -0500

    fixed dark mode issue

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "81164a99d0515997"; }
+/******/ 		__webpack_require__.h = function() { return "2bb946bf5d8b23e6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
