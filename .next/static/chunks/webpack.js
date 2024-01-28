81f3635ce0f39ec2026168d1e7afe81fff196b87
egwetzel <emmegwetzel@gmail.com>
2024-01-28 15:35:11 -0600

    Updated gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d0e5d06b3c3d83fb"; }
+/******/ 		__webpack_require__.h = function() { return "5aa9c35f94708005"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
